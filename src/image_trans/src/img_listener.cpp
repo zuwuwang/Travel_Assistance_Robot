84ea77a999961b81b307ec09328177dbb6ad0110
wzw <1055259415@qq.com>
2018-05-12 13:24:43 +0800

    1.project frame ok 2.image_trans node can save img_node images,and display in namewindows 3.socket transfer ok

---
+++ src/image_trans/src/img_listener.cpp
@@ -0,0 +1,237 @@
+/*****
+ * sub img from img topic:/camera/image_raw
+ * *****/
+//qt head
+
+/***
+ * ros img headfile
+ * ****/
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <iostream>
+#include <sys/time.h>
+
+using namespace std;
+/****
+ * socket headfile
+ * ******/
+#include <netinet/in.h>    // for sockaddr_in
+#include <sys/types.h>    // for socket
+#include <sys/socket.h>    // for socket
+#include <stdio.h>        // for qDebug
+#include <stdlib.h>        // for exit
+#include <string.h>        // for bzero
+#include <time.h>                //for time_t and time
+#include <arpa/inet.h>
+#include <unistd.h>    //close(client_socket);
+
+// opencv headfile
+#include <vector>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+#define SERVER_PORT   7754
+#define BUFFER_SIZE 1024
+#define serverIpAddr "192.168.1.101"
+
+unsigned int fileNum = 1;
+bool imageSaveFlag;
+void imageTransCallback(const sensor_msgs::ImageConstPtr& msg);
+
+// main function
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "img_listener");
+  ros::NodeHandle nhImage;
+  cv::namedWindow("show image");
+  cv::startWindowThread();
+  image_transport::ImageTransport transport(nhImage);
+  image_transport::Subscriber sub = transport.subscribe("/camera/image_raw",1,imageTransCallback);
+
+  //ros::spin() 在调用后不会再返回，也就是主程序到这儿就不往下执行了
+  ros::spin();
+  cv::destroyWindow("view");
+
+  return 0;
+}
+
+void imageTransCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    //get img & show
+    cv::Mat src_image = cv_bridge::toCvShare(msg, "bgr8")->image;
+    cv::imshow("show image", src_image);
+    //imgsave
+    char key;
+    key=cv::waitKey(33);
+
+    struct tm* fileTime;
+               char filePath[100];
+               char fileName[100];
+               time_t t;
+               t=time(NULL);
+               fileTime=localtime(&t);
+               strftime(filePath,128,"/home/nvidia/Travel_Assistance_Robot/image/img_%Y%m%d_%H%M%S.jpg",fileTime);
+               strftime(fileName,128,"img_%Y%m%d_%H%M%S.jpg",fileTime);
+
+    // wait for space
+    if(key==32)
+      imageSaveFlag = true;
+    if(imageSaveFlag)
+    {
+      cv::imwrite(filePath,cv_bridge::toCvShare(msg, "bgr8")->image);
+      imageSaveFlag = false;
+      printf(" had saved %s ",fileName);
+    }
+//   //socket image trans
+//    {
+//      /**************
+//       * socket struct
+//       * ***********/
+//      //设置一个socket地址结构client_addr,代表客户机internet地址, 端口
+//      struct sockaddr_in client_addr;
+//      bzero(&client_addr,sizeof(client_addr)); //把一段内存区的内容全部设置为0
+//     // memset(&client_addr,sizeof(client_addr));
+//      client_addr.sin_family = AF_INET;    //internet协议族
+//      client_addr.sin_addr.s_addr = htons(INADDR_ANY);//INADDR_ANY表示自动获取本机地址
+//      client_addr.sin_port = htons(0);    //0表示让系统自动分配一个空闲端口
+
+//      /*****
+//       * socket descriptor
+//       * 创建用于internet的流协议(TCP)socket,用client_socket代表客户机socket
+//       * ****/
+//      int client_socket = socket(AF_INET,SOCK_STREAM,0);
+//      if( client_socket < 0)
+//          {
+//              cout<<"Create Socket Failed!"<<endl;
+//              exit(1);
+//          }
+
+//      /******
+//       * bind port
+//       * 把客户机的socket和客户机的socket地址结构联系起来,zhiding client_socket ip,point to addr
+//       * *******/
+//      if( bind(client_socket,(struct sockaddr*)&client_addr,sizeof(client_addr)))
+//          {
+//              cout<<"Client Bind Port Failed!"<<endl;
+//              exit(1);
+//          }
+
+//      /*****
+//       * get server addr from argv[1],
+//       * set server params
+//       * 设置一个socket地址结构server_addr,代表服务器的internet地址, 端口
+//       * server_addr.sin_addr.s_addr=inet_addr(argv[1]);//same as up inet_aton
+//       * ****/
+//      //
+//      struct sockaddr_in server_addr;
+//      bzero(&server_addr,sizeof(server_addr));
+//      server_addr.sin_family = AF_INET;
+//      server_addr.sin_addr.s_addr = inet_addr(serverIpAddr);
+//      server_addr.sin_port = htons(SERVER_PORT);
+//      socklen_t server_addr_length = sizeof(server_addr);
+//      /***
+//       * connect request,to server ip.success return 0;fail is 1
+//       * 向服务器发起连接,连接成功后client_socket代表了客户机和服务器的一个socket连接
+//       * **/
+//       if(connect(client_socket,(struct sockaddr*)&server_addr, server_addr_length) < 0)
+//          {
+//              cout<<"Can Not Connect To %s!"<<serverIpAddr<<endl;
+//              exit(1);
+//          }
+//      cout<<"success connect To %s!"<<serverIpAddr<<endl;
+
+//      /*********************  data transfer test  ****************************/
+//      /*****
+//       * data prepare,set buffer
+//       * bzero == memset
+//       * ****/
+//      char buffer[BUFFER_SIZE];
+//      bzero(buffer,BUFFER_SIZE);
+//      //从服务器接收数据到buffer中
+//      int length = recv(client_socket,buffer,BUFFER_SIZE,0);
+//      if(length < 0)
+//          {
+//              cout<<"Recieve Data From Server %s Failed!"<<serverIpAddr<<endl;
+//              exit(1);
+//          }
+//      cout<<"buffer size is "<<buffer<<endl;
+//      bzero(buffer,BUFFER_SIZE);
+//      // 向服务器发buffer中的数据
+//      bzero(buffer,BUFFER_SIZE);
+//      strcpy(buffer,"Hello, World! From Client\n");
+//      int send_flag=send(client_socket,buffer,BUFFER_SIZE,0);
+//      if(!send_flag)
+//          cout<<"send error"<<endl;
+//      cout<<"send success"<<endl;
+
+//      /********************* 向服务器发送image  ****************************/
+//      //1.load image,get imagesize
+//      cv::Mat s_img=cv::imread("1.jpg");
+//      imshow("s_img",s_img);
+//      vector<uchar> encode_img;
+//      imencode(".jpg", s_img, encode_img);
+//      //get send_buffer
+//      int encode_img_size=encode_img.size();
+//      int s_img_size=s_img.rows*s_img.cols*3;
+//      cout<<"filesize is"<<encode_img_size<< "width*hight*3 is"<<s_img_size<<endl;
+
+//      uchar* send_buffer=new uchar[encode_img.size()];
+//      copy(encode_img.begin(),encode_img.end(),send_buffer);
+//      //2.send file_name
+//      int toSend=encode_img_size, receive  = 0, finished = 0;
+//      const char* file_name;
+//      char char_len[10];
+//      file_name= fn.c_str();
+//      // file_name,qDebug file_name be empty
+//      //qDebug("file name is %s\n",file_name);
+//      bzero(buffer,BUFFER_SIZE);
+//      send_flag=send(client_socket, file_name, 10, 0);
+//      if(!send_flag)
+//        {
+//          cout<<" send file_name failed\n "<<endl;
+//          exit(1);
+//        }
+//      cout<<"success send file_name"<<endl;
+//      //3.send image length
+//      sprintf(char_len, "%d", toSend);
+//      send(client_socket, char_len, 10, 0);//hello world!!hei hei(xiao)!!  strlen(char_len)这里要写一个固定长度，然后让服务器端读出一个固定长度，否则会出错
+//      cout<<"char_len is "<<char_len<<endl;
+
+//      // send test
+
+//      //4.send image data
+//      while(toSend  >  0)
+//      {
+//          int size = (toSend>1000?1000:toSend);
+//          if((receive = send(client_socket, send_buffer + finished, size, 0)))  //send wenzi
+//          {
+//              if(receive==-1)
+//              {
+//                  cout<<"receive error"<<endl;
+//                  break;
+//              }
+//              else
+//              {
+//                  toSend -= receive;// shengxia de unsend
+//                  finished += receive; //sended
+//              }
+//          }
+//      }
+
+//      //5.close socket
+//      close(client_socket);
+//      cout<<"close socket"<<endl;
+//    }
+
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
---
+++ src/image_trans/src/img_send.cpp
@@ -0,0 +1,168 @@
+/***
+ * sub img,socket transfer
+ * **********/
+//Includes all the headers necessary to use the most common public pieces of the ROS system.
+#include <ros/ros.h>
+//Use image_transport for publishing and subscribing to images in ROS
+#include <image_transport/image_transport.h>
+//Use cv_bridge to convert between ROS and OpenCV Image formats
+#include <cv_bridge/cv_bridge.h>
+
+#include <sensor_msgs/image_encodings.h>
+//Include headers for OpenCV Image processing
+#include <opencv2/imgproc/imgproc.hpp>
+//Include headers for OpenCV GUI handling
+#include <opencv2/highgui/highgui.hpp>
+#include<string>
+#include <sstream>
+#include <std_msgs/UInt8.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include  <arpa/inet.h>
+using namespace cv;
+using namespace std;
+
+//Store all constants for image encodings in the enc namespace to be used later.
+namespace enc = sensor_msgs::image_encodings;
+
+void image_socket(Mat inImg);
+Mat image1;
+static int imgWidth, imgHeight;
+
+static int image_num = 1;
+
+#define PORT 30000
+#define BUFFER_SIZE 20000
+const char * servInetAddr = "10.95.6.210"; //service addr
+static int sockfd;
+
+//static ros::Publisher capture;
+//This function is called everytime a new image_info message is published
+void camInfoCallback(const sensor_msgs::CameraInfo & camInfoMsg)
+{
+  //Store the image width for calculation of angle
+  imgWidth = camInfoMsg.width;
+  imgHeight = camInfoMsg.height;
+}
+
+//This function is called everytime a new image is published
+void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
+{
+    //Convert from the ROS image message to a CvImage suitable for working with OpenCV for processing
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        //Always copy, returning a mutable CvImage
+        //OpenCV expects color images to use BGR channel order.
+        cv_ptr = cv_bridge::toCvCopy(original_image, enc::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        //if there is an error during conversion, display it
+        ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());
+        return;
+    }
+    image_socket(cv_ptr->image);
+
+}
+
+void image_socket(Mat inImg)
+{
+   imshow("image_socket", inImg);//显示图片
+    if( inImg.empty() )
+    {
+      ROS_INFO("Camera image empty");
+      return;//break;
+    }
+    stringstream sss;
+    string strs;
+
+    char c = (char)waitKey(1);
+
+    if( c == 27 )
+      ROS_INFO("Exit boss");//break;
+    switch(c)
+    {
+      case 'p':
+
+      resize(inImg,image1,Size(imgWidth/6,imgHeight/6),0,0,CV_INTER_LINEAR);
+      image1=image1(Rect(image1.cols/2-32,image1.rows/2-32, 64, 64));
+
+      strs="/home/hsn/catkin_ws/src/rosopencv/";
+      sss.clear();
+      sss<<strs;
+      sss<<image_num;
+      sss<<".jpg";
+      sss>>strs;
+      imwrite(strs,image1);//保存图片
+      cout<<image_num<<endl;
+
+      int n;
+      n = write(sockfd,image1.data,image1.total()*image1.channels());
+      if (n < 0)
+         perror("ERROR writing to socket");
+      image_num++;
+      break;
+
+  default:
+      break;
+  }
+
+}
+
+/**
+* This is ROS node to track the destination image
+*/
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "image_socket");
+    ROS_INFO("-----test------");
+
+    struct sockaddr_in serv_addr;
+    /*创建socket*/
+    if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1)
+    {
+      perror("Socket failed!\n");
+      exit(1);
+    }
+    printf("Socket id = %d\n",sockfd);
+    /*设置sockaddr_in 结构体中相关参数*/
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, servInetAddr, &serv_addr.sin_addr);
+    bzero(&(serv_addr.sin_zero), 8);
+    /*调用connect 函数主动发起对服务器端的连接*/
+    if(connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr))== -1)
+    {
+        perror("Connect failed!\n");
+        exit(1);
+    }
+    printf("welcome\n");
+
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+
+    image_transport::Subscriber sub = it.subscribe("camera/rgb/image_raw", 1, imageCallback);
+    ros::Subscriber camInfo         = nh.subscribe("camera/rgb/camera_info", 1, camInfoCallback);
+
+    ros::Rate loop_rate(10);
+
+    while (ros::ok())
+    {
+       ros::spinOnce();
+       loop_rate.sleep();
+    }
+    close(sockfd);
+    //ROS_INFO is the replacement for printf/cout.
+    ROS_INFO("No error.");
+return 0;
+
+}
